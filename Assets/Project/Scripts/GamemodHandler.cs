60955ba2b0bdc1f137b0b9e212feffaa43c5a6ca
AshenBushi <61754110+AshenBushi@users.noreply.github.com>
2021-06-22 09:22:59 +0300

    Add second view

--- Assets/Project/Scripts/GamemodHandler.cs
+++ Assets/Project/Scripts/GamemodHandler.cs
@@ -8,6 +8,7 @@ public class GamemodHandler : MonoBehaviour
 {
     [SerializeField] private List<Button> _buttons;
     [SerializeField] private float _duration;
+    [SerializeField] private Animator _cameraAnimator;
 
     [Header("Background Settings")] 
     [SerializeField] private Image _background;
@@ -17,6 +18,7 @@ public class GamemodHandler : MonoBehaviour
     [SerializeField] private Vector3 _unselected;
 
     private Tween _tween;
+    private int _cameraViewIndex = 1;
 
     public event UnityAction IsModChanged;
 
@@ -25,28 +27,46 @@ public class GamemodHandler : MonoBehaviour
         SelectMod(DataManager.GameData.ProgressData.CurrentGamemod, 0f);
     }
 
+    private void SelectBackground(int index)
+    {
+        _background.sprite = _backgroundSprites[index];
+    }
+
+    private void TryChangeCameraView(int index)
+    {
+        if (index < 3)
+        {
+            if (_cameraViewIndex == 1) return;
+            _cameraAnimator.SetTrigger("EnableFirstView");
+            _cameraViewIndex = 1;
+        }
+        else
+        {
+            if (_cameraViewIndex == 2) return;
+            _cameraAnimator.SetTrigger("EnableSecondView");
+            _cameraViewIndex = 2;
+        }
+    }
+    
     private void SelectMod(int index, float duration)
     {
-        SelectBackground(index);
-        
+        SelectBackground(index % 3);
+        TryChangeCameraView(index);
+
         for (var i = 0; i < _buttons.Count; i++)
         {
             _tween = _buttons[i].transform.DOScale(i == index ? _selected : _unselected, duration);
         }
     }
 
-    private void SelectBackground(int index)
-    {
-        _background.sprite = _backgroundSprites[index];
-    }
-    
     public void SelectMod(int index)
     {
         SoundManager.PlaySound(SoundNames.ButtonClick);
-        DataManager.GameData.ProgressData.CurrentGamemod = index;
+        DataManager.GameData.ProgressData.CurrentGamemod = index % 3;
         DataManager.Save();
         
-        SelectBackground(index);
+        SelectBackground(index % 3);
+        TryChangeCameraView(index);
         
         for (var i = 0; i < _buttons.Count; i++)
         {
