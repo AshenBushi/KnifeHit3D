08a1ba523d9e58221f7d6a5ca97631876b1cc9d1
lemurrqa <65906359+lemurrqa@users.noreply.github.com>
2021-11-02 17:40:39 +0300

    Change distance between walls

--- Assets/_Project/KnifeFest/Scripts/WallSpawner.cs
+++ Assets/_Project/KnifeFest/Scripts/WallSpawner.cs
@@ -8,16 +8,21 @@ namespace KnifeFest
 {
     public class WallSpawner : Singleton<WallSpawner>
     {
+        private const float wallOffset = 30f;
+
         [SerializeField] private PathCreator _path;
         [SerializeField] private Transform _road;
         [SerializeField] private Wall _template;
         [SerializeField] private float _leftX;
         [SerializeField] private float _rightX;
+        [SerializeField] private Camera _camera;
 
         private readonly List<Wall> _walls = new List<Wall>();
         private MeshRenderer _meshRoad;
 
         public List<Wall> Walls => _walls;
+
+        public static float WallOffset => wallOffset;
 
         protected override void Awake()
         {
@@ -31,15 +36,15 @@ namespace KnifeFest
             for (var i = 0; i < level.Walls.Count; i++)
             {
                 var wall = level.Walls[i];
-                Walls.Add(Instantiate(_template, new Vector3(wall.Side == Side.Left ? _leftX : _rightX, 1.5f, wall.ZPosition * 20f), Quaternion.Euler(Vector3.zero), transform));
+                Walls.Add(Instantiate(_template, new Vector3(wall.Side == Side.Left ? _leftX : _rightX, 1.5f, wall.ZPosition * wallOffset), Quaternion.Euler(Vector3.zero), transform));
                 Walls[i].Init(wall.Type, wall.value);
 
-                if (greatestZ < wall.ZPosition)
-                    greatestZ = wall.ZPosition;
+                if (greatestZ < _camera.WorldToScreenPoint(Walls[i].transform.position).z)
+                    greatestZ = _camera.WorldToScreenPoint(Walls[i].transform.position).z;
             }
 
-            _road.localScale = new Vector3(1f, 1f, greatestZ + 1f);
-            _path.bezierPath.AddSegmentToEnd(new Vector3(0f, 0f, (greatestZ + 0.5f) * 20f));
+            _road.localScale = new Vector3(1f, 1f, greatestZ / 17f);
+            _path.bezierPath.AddSegmentToEnd(new Vector3(0f, 0f, (greatestZ / (wallOffset / 1.1f) + 0.5f) * wallOffset));
 
             FinalCutscene.OnCreatingCurscene?.Invoke();
             _meshRoad = _road.GetComponent<MeshRenderer>();
